ab681e3bc86774bf6fa4d5642af5bdb9d9434d60
John Paul Al-haddi <jpaul.alhaddi@gmail.com>
2024-07-08 03:04:36 +0800

    Add: services

--- script.js
+++ script.js
@@ -12,16 +12,25 @@ document.addEventListener('DOMContentLoaded', function () {
   });
 
   // Smooth scroll with offset
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
         e.preventDefault();
 
         const target = document.querySelector(this.getAttribute('href'));
+        const isMobile = window.innerWidth <= 768; 
+
         if (target) {
+            let offset = 0;
+            if (!isMobile) {
+                const navbarHeight = document.querySelector('.custom-navbar').offsetHeight; 
+                offset = navbarHeight;
+            }
+
             window.scrollTo({
-                top: target.offsetTop - 0,
+                top: target.offsetTop - offset,
                 behavior: 'smooth'
             });
         }
     });
 });
+
