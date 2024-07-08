079a55edbecba045ebcec4b22d7ed2cdffd25dec
John Paul Al-haddi <jpaul.alhaddi@gmail.com>
2024-07-08 23:51:44 +0800

    Add: Scroll animation

--- script.js
+++ script.js
@@ -66,3 +66,16 @@ function updateNavbar() {
 updateNavbar();
 
 window.addEventListener('scroll', updateNavbar);
+
+
+const observer = new IntersectionObserver((entries, observer) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('show');
+      observer.unobserve(entry.target); // Stop observing the element after adding the 'show' class
+    }
+  });
+});
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
